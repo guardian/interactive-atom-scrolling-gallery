b791e97430cde71c2647f248f6e63efdfd9cd4a5
Catriona Morrison <catriona.morrison@guardian.co.uk>
2021-12-15 14:44:38 +0000

    Hooking up to external data sheet

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,2 +1,52 @@
 // if you want to import a module from shared/js then you can
 // just do e.g. import Scatter from "shared/js/scatter.js"
+import Swiper from "swiper";
+
+var fastSpeed = 400;
+var slowSpeed = 12000;
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    centeredSlides: false,
+    spaceBetween: 1,
+    loop: true,
+    grabCursor: true,
+    freeMode: true,
+    roundLengths: true,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.2,
+            spaceBetween: 1
+        },
+        660: {
+            slidesPerView: 1.6,
+            spaceBetween: 1
+        },
+        980: {
+            slidesPerView: 2.1,
+            spaceBetween: 1
+        }
+    },
+    speed: slowSpeed
+});
+
+
+if (isAndroidApp && window.GuardianJSInterface.registerRelatedCardsTouch) {
+
+    i = 0;
+
+    swiper.forEach(swipe => {
+        var index = i;
+        swiper[index].wrapperEl.addEventListener("touchstart", function () {
+            window.GuardianJSInterface.registerRelatedCardsTouch(true);
+        });
+        swiper[index].wrapperEl.addEventListener("touchend", function () {
+            window.GuardianJSInterface.registerRelatedCardsTouch(false);
+        });
+        i++;
+    });
+}
--- atoms/default/server/render.js
+++ atoms/default/server/render.js
@@ -1,5 +1,15 @@
-import mainHTML from "./atoms/default/server/templates/main.html!text"
+import mainTemplate from "./atoms/default/server/templates/main.html!text"
+import Mustache from 'mustache'
+import rp from 'request-promise'
 
-export async function render() {
-    return mainHTML;
-} 
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata-test/11B90r_OGevVT1L6t02pN9GuASfhVGiYCs_gg_-j-mgI.json',
+        json: true
+    }).then((data) => {
+        var sheets = data.sheets;
+        console.log(sheets);
+        var html = Mustache.render(mainTemplate, sheets);
+        return html;
+    });
+}
