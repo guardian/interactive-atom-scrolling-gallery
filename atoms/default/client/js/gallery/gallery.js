4d1cddabb31d5913b7256379d387bc090def0b74
Catriona Morrison <catriona.morrison@guardian.co.uk>
2022-01-18 11:33:55 +0000

    decoupling gallery and dragging

--- atoms/default/client/js/gallery/gallery.js
+++ atoms/default/client/js/gallery/gallery.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-param-reassign */
+import Dragging from './galleryDragging';
+import events from '../../events-emitter';
+
 export default class Gallery {
   constructor() {
     this.galleryContainer = document.querySelector('.gallery-container');
@@ -6,40 +9,29 @@ export default class Gallery {
     this.galleryImages = document.querySelectorAll('.gallery-slide');
     this.galleryButton = document.querySelector('.gallery-button');
 
-    this.initialSpeed = 40;
-    this.speed = this.initialSpeed;
     this.playingGallery = false;
     this.mouseDown = false;
-    this.scroll = 0;
-    this.initialLeft = this.galleryWrapper.getBoundingClientRect().left;
-    this.galleryInitialLength = this.galleryWrapper.offsetWidth;
 
-    this.galleryWrapper.style.setProperty('--gallery-width', `-${this.galleryWrapper.offsetWidth}px`);
     this.createImagesDuplicatesToAllowLoopingGallery();
     this.galleryStart();
-    // this.addDraggingFunctionalityToGallery();
 
-    // this.galleryWrapper.addEventListener('animationend', (e) => this.resetAnimation(e));
-    this.galleryWrapper.addEventListener('animationiteration', (e) => this.resetAnimation(e));
     this.galleryButton.addEventListener('click', () => (this.playingGallery ? this.galleryPause() : this.galleryPlay()));
-  }
 
-  resetAnimation(e) {
-    e.preventDefault();
-    if (this.galleryWrapper.classList.contains('second')) {
-      this.galleryWrapper.classList.remove('second');
-      this.speed = this.initialSpeed;
-      this.galleryWrapper.style.setProperty('--gallery-speed-resume', `${this.speed}s`);
-      this.galleryWrapper.classList.add('first');
+    this.draggingEnabled = false;
+    if (this.draggingEnabled) {
+      this.dragging = new Dragging();
+      this.dragging.addDraggingFunctionalityToGallery();
+
+      events.on('galleryDragging-pause', () => {
+        this.galleryPause();
+      });
     }
-    this.galleryPlayingInitialTime = new Date().getTime();
-    this.scroll = 0;
   }
 
   galleryStart() {
     this.galleryWrapper.classList.add('first');
     this.playingGallery = true;
-    this.galleryPlayingInitialTime = new Date().getTime();
+    events.emit('gallery-start');
   }
 
   galleryPause() {
@@ -47,6 +39,7 @@ export default class Gallery {
     document.querySelector('.gallery-button--play').classList.add('active');
     document.querySelector('.gallery-button--pause').classList.remove('active');
     this.playingGallery = false;
+    events.emit('gallery-pause');
   }
 
   galleryPlay() {
@@ -55,6 +48,7 @@ export default class Gallery {
     document.querySelector('.gallery-button--play').classList.remove('active');
     document.querySelector('.gallery-button--pause').classList.add('active');
     this.playingGallery = true;
+    events.emit('gallery-play');
   }
 
   createImagesDuplicatesToAllowLoopingGallery() {
@@ -73,57 +67,6 @@ export default class Gallery {
       newImage.classList.add('gallery-slide-duplicate');
       newImage.classList.remove('gallery-slide');
       this.galleryWrapper.appendChild(newImage);
-    });
-  }
-
-  translateTo() {
-    this.time = new Date().getTime() - this.galleryPlayingInitialTime;
-    const progress = this.time / (this.speed * 1000);
-    const currentPosition = progress * this.galleryWrapper.offsetWidth + this.translate;
-    return currentPosition;
-  }
-
-  startDragging(e) {
-    e.preventDefault();
-    this.mouseDown = true;
-    // eslint-disable-next-line max-len
-    this.initialLocale = e.pageX;
-    this.translate = -this.scroll;
-    if (this.playingGallery) {
-      this.galleryWrapper.classList.remove('first');
-      this.galleryWrapper.classList.remove('second');
-      this.galleryPause();
-      this.translate = this.translateTo(e);
-      this.galleryWrapper.style.transform = `translate3d(-${this.translate}px, 0, 0)`;
-    }
-  }
-
-  dragging(e) {
-    e.preventDefault();
-    if (!this.mouseDown) return;
-    this.scroll = e.pageX - this.translate - this.initialLocale;
-    this.galleryWrapper.style.transform = `translate3d(${this.scroll}px, 0, 0)`;
-  }
-
-  stopDragging() {
-    this.mouseDown = false;
-    this.galleryWrapper.style.setProperty('--gallery-current-location', `${this.scroll}px`);
-    // eslint-disable-next-line max-len
-    this.speed = Math.round(this.initialSpeed + (this.scroll / this.galleryInitialLength) * this.initialSpeed);
-    this.galleryWrapper.style.setProperty('--gallery-speed-resume', `${this.speed}s`);
-    this.galleryWrapper.classList.add('second');
-  }
-
-  addDraggingFunctionalityToGallery() {
-    document.querySelectorAll('.slides').forEach((img) => {
-      img.addEventListener('touchstart', this.startDragging.bind(this), false);
-      img.addEventListener('mousedown', this.startDragging.bind(this), false);
-
-      img.addEventListener('touchmove', this.dragging.bind(this), false);
-      img.addEventListener('mousemove', this.dragging.bind(this), false);
-
-      img.addEventListener('touchend', this.stopDragging.bind(this), false);
-      img.addEventListener('mouseup', this.stopDragging.bind(this), false);
     });
   }
 }
---
+++ atoms/default/client/js/gallery/galleryDragging.js
@@ -0,0 +1,94 @@
+import events from '../../events-emitter';
+
+export default class Dragging {
+  constructor() {
+    this.galleryWrapper = document.querySelector('.gallery-wrapper');
+    this.galleryPlayingInitialTime = 0;
+    this.initialSpeed = 40;
+    this.speed = this.initialSpeed;
+    this.galleryPlaying = false;
+
+    this.scroll = 0;
+
+    this.initialLeft = this.galleryWrapper.getBoundingClientRect().left;
+    this.galleryInitialLength = this.galleryWrapper.offsetWidth;
+
+    this.galleryWrapper.style.setProperty('--gallery-width', `-${this.galleryWrapper.offsetWidth}px`);
+
+    this.galleryWrapper.addEventListener('animationend', (e) => this.resetAnimation(e));
+    this.galleryWrapper.addEventListener('animationiteration', (e) => this.resetAnimation(e));
+    events.on('gallery-start', () => {
+      this.galleryPlaying = true;
+      this.galleryPlayingInitialTime = new Date().getTime();
+    });
+    events.on('gallery-play', () => {
+      this.galleryPlaying = true;
+      this.galleryPlayingInitialTime = new Date().getTime();
+    });
+    events.on('gallery-pause', () => {
+      this.galleryPlaying = false;
+    });
+  }
+
+  addDraggingFunctionalityToGallery() {
+    document.querySelectorAll('.slides').forEach((img) => {
+      img.addEventListener('touchstart', this.startDragging.bind(this), false);
+      img.addEventListener('mousedown', this.startDragging.bind(this), false);
+
+      img.addEventListener('touchmove', this.dragging.bind(this), false);
+      img.addEventListener('mousemove', this.dragging.bind(this), false);
+
+      img.addEventListener('touchend', this.stopDragging.bind(this), false);
+      img.addEventListener('mouseup', this.stopDragging.bind(this), false);
+    });
+  }
+
+  resetAnimation(e) {
+    e.preventDefault();
+    if (this.galleryWrapper.classList.contains('second')) {
+      this.galleryWrapper.classList.remove('second');
+      this.speed = this.initialSpeed;
+      this.galleryWrapper.style.setProperty('--gallery-speed-resume', `${this.speed}s`);
+      this.galleryWrapper.classList.add('first');
+    }
+    this.galleryPlayingInitialTime = new Date().getTime();
+    this.scroll = 0;
+  }
+
+  translateTo() {
+    this.time = new Date().getTime() - this.galleryPlayingInitialTime;
+    const progress = this.time / (this.speed * 1000);
+    const currentPosition = progress * this.galleryWrapper.offsetWidth + this.translate;
+    return currentPosition;
+  }
+
+  startDragging(e) {
+    e.preventDefault();
+    this.mouseDown = true;
+    // eslint-disable-next-line max-len
+    this.initialLocale = e.pageX;
+    this.translate = -this.scroll;
+    this.galleryWrapper.classList.remove('first');
+    this.galleryWrapper.classList.remove('second');
+    events.emit('galleryDragging-pause');
+    // if paused translate should just be previous scroll location
+    this.translate = this.galleryPlaying ? this.translateTo(e) : this.translate;
+    this.galleryWrapper.style.transform = `translate3d(-${this.translate}px, 0, 0)`;
+  }
+
+  dragging(e) {
+    e.preventDefault();
+    if (!this.mouseDown) return;
+    this.scroll = e.pageX - this.translate - this.initialLocale;
+    this.galleryWrapper.style.transform = `translate3d(${this.scroll}px, 0, 0)`;
+  }
+
+  stopDragging() {
+    this.mouseDown = false;
+    this.galleryWrapper.style.setProperty('--gallery-current-location', `${this.scroll}px`);
+    // eslint-disable-next-line max-len
+    this.speed = Math.round(this.initialSpeed + (this.scroll / this.galleryInitialLength) * this.initialSpeed);
+    this.galleryWrapper.style.setProperty('--gallery-speed-resume', `${this.speed}s`);
+    this.galleryWrapper.classList.add('second');
+  }
+}
